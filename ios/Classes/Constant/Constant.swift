db593fa601460fdd65381dc780a00605f8889d4e
Phuong <tdphuong96@gmail.com>
2022-12-12 17:35:47 +0700

    + update func ios

---
+++ ios/Classes/Constant/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  omicallsdk
+//
+//  Created by LV on 12/12/2022.
+//
+
+import Foundation
+
+
+static let INIT_CALL = "INIT_CALL";
+static let UPDATE_TOKEN = "UPDATE_TOKEN";
+static let START_OMI_SERVICE = "START_OMI_SERVICE";
+static let START_CALL = "START_CALL";
+static let END_CALL = "END_CALL";
+static let TOGGLE_MUTE = "TOGGLE_MUTE";
+static let TOGGLE_SPEAK = "TOGGLE_SPEAK";
+static let CHECK_ON_GOING_CALL = "CHECK_ON_GOING_CALL";
+static let DECLINE = "DECLINE";
+static let FORWARD_CALL_TO = "FORWARD_CALL_TO";
+static let HANGUP = "HANGUP";
+static let ON_CALL_STARTED = "ON_CALL_STARTED";
+static let ON_HOLD = "ON_HOLD";
+static let ON_IN_COMING_RECEIVE = "ON_IN_COMING_RECEIVE";
+static let ON_MUTE = "ON_MUTE";
+static let ON_OUT_GOING = "ON_OUT_GOING";
+static let ON_OUT_GOING_STARTED = "ON_OUT_GOING";
+static let REGISTER = "REGISTER";
