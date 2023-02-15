cae5bae2a1458f7f2df74180c6a3d30e94282b78
Steve <chaunguyen4297@gmail.com>
2023-02-15 21:44:51 +0700

    feat<native> add callback from native <chaunn>

---
+++ android/src/main/kotlin/vn/vihat/omicall/omicallsdk/video_call/fl_local_camera_factory.kt
@@ -0,0 +1,13 @@
+package vn.vihat.omicall.omicallsdk.video_call
+
+import android.content.Context
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+class FLLocalCameraFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        val creationParams = args as Map<String?, Any?>?
+        return FLLocalCameraView(context, viewId, creationParams)
+    }
+}
--- ios/Classes/VideoCall/FLLocalCameraView.swift
+++ ios/Classes/VideoCall/FLLocalCameraView.swift
@@ -17,7 +17,11 @@ class FLLocalCameraFactory: NSObject, FlutterPlatformViewFactory {
         self.messenger = messenger
         super.init()
     }
-
+    
+    func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
+        return FlutterStandardMessageCodec.sharedInstance()
+    }
+    
     func create(
         withFrame frame: CGRect,
         viewIdentifier viewId: Int64,
@@ -34,6 +38,8 @@ class FLLocalCameraFactory: NSObject, FlutterPlatformViewFactory {
 
 class FLLocalCameraView: NSObject, FlutterPlatformView {
     private var _view: UIView
+    private var _arg : [String : Any]?
+    private let methodChannel: FlutterMethodChannel?
 
     init(
         frame: CGRect,
@@ -42,12 +48,24 @@ class FLLocalCameraView: NSObject, FlutterPlatformView {
         binaryMessenger messenger: FlutterBinaryMessenger?
     ) {
         _view = UIView()
+        _arg = args as? [String: Any]
+        methodChannel = FlutterMethodChannel(name: "local_camera_controller/\(viewId)", binaryMessenger: messenger!)
         super.init()
+        methodChannel?.setMethodCallHandler(onMethodCall)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {[weak self] in
             guard let self = self else { return }
             self.setupViews()
         })
     }
+    
+    func onMethodCall(call: FlutterMethodCall, result: FlutterResult) {
+            switch(call.method){
+            case "":
+                break
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
 
     func view() -> UIView {
         return _view
@@ -67,6 +85,20 @@ class FLLocalCameraView: NSObject, FlutterPlatformView {
         child.addSubview(bottomView)
         bottomView.frame = CGRect(x: 0, y: height / 2, width: width, height: height / 2)
         bottomView.backgroundColor = .red
+        //create text
+        let text = UILabel()
+        topView.addSubview(text)
+        text.text = _arg?["title"] as! String
+        text.frame = CGRect(x: 15, y: 15, width: width - 30, height: 20)
+        text.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(clickText)))
+        text.isUserInteractionEnabled = true
+    }
+    
+    @objc func clickText() {
+        print("aaaa")
+        if let channel = methodChannel {
+            channel.invokeMethod("click", arguments: nil)
+        }
     }
     
 }
